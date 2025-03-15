72d47770156acdaaf956f782e3c6eebb716a34a7
TyShelbyCrenshaw <linxtree@gmail.com>
2025-03-14 21:38:37 -0600

    remove sounds and refs

--- src/music/audioIndex.js
+++ src/music/audioIndex.js
@@ -1,16 +1,10 @@
 /* eslint-disable */
 import forever_live_sessions_vol_2_hq from './processed/forever_live_sessions_vol_2_hq/index.js';
 import the_bug_official_video from './processed/the_bug_official_video/index.js';
-import _cande_roldan__pinback____offcell__2003__full_ep from './processed/_cande_roldan__pinback____offcell__2003__full_ep/index.js';
-import _cande_roldan__pinback____loro__1999__full_ep from './processed/_cande_roldan__pinback____loro__1999__full_ep/index.js';
-import _kryph__pinback___too_many_shadows__full_ep_ from './processed/_kryph__pinback___too_many_shadows__full_ep_/index.js';
 
 // Export all tracks
 export default {
   forever_live_sessions_vol_2_hq,
   the_bug_official_video,
-  _cande_roldan__pinback____offcell__2003__full_ep,
-  _cande_roldan__pinback____loro__1999__full_ep,
-  _kryph__pinback___too_many_shadows__full_ep_
 };
 /* eslint-enable */
--- src/music/trackInfo.js
+++ src/music/trackInfo.js
@@ -12,24 +12,6 @@ export default [
     title: "The Bug Official Video",
     fileName: "the-bug-official-video.mp3",
     artist: "Unknown" // You may want to extract this from ID3 tags in the future
-  },
-  {
-    id: "_cande_roldan__pinback____offcell__2003__full_ep",
-    title: "[Cande Roldan] Pinback  – Offcell (2003) FULL EP",
-    fileName: "[Cande Roldan] Pinback _– Offcell (2003) FULL EP.mp3",
-    artist: "Unknown" // You may want to extract this from ID3 tags in the future
-  },
-  {
-    id: "_cande_roldan__pinback____loro__1999__full_ep",
-    title: "[Cande Roldan] Pinback  — Loro (1999) FULL EP",
-    fileName: "[Cande Roldan] Pinback _— Loro (1999) FULL EP.mp3",
-    artist: "Unknown" // You may want to extract this from ID3 tags in the future
-  },
-  {
-    id: "_kryph__pinback___too_many_shadows__full_ep_",
-    title: "[Kryph] Pinback   Too Many Shadows (Full EP)",
-    fileName: "[kryph] Pinback - Too Many Shadows (Full EP).mp3",
-    artist: "Unknown" // You may want to extract this from ID3 tags in the future
   }
 ];
 /* eslint-enable */
